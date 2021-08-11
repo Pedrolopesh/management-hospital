e9290ac8e984f2378be60831681b9d6375303722
Pedrolopesh <47143677+Pedrolopesh@users.noreply.github.com>
2021-08-11 18:31:24 -0300

    Update de classes unidadeHospitalar e Equipe

--- src/main/java/com/management/Equipe.java
+++ src/main/java/com/management/Equipe.java
@@ -1,9 +1,19 @@
 package com.management;
 
+import java.util.ArrayList;
+
 public class Equipe {
     private String nomeEquipe;
     private int idEquipe;
     private String liderEquipe;
+    private ArrayList<Funcionario> funcionarios;
+
+    public Equipe(String nomeEquipe, int idEquipe, String liderEquipe){
+        this.nomeEquipe = nomeEquipe;
+        this.idEquipe = idEquipe;
+        this.liderEquipe = liderEquipe;
+        this.funcionarios = new ArrayList<Funcionario>();
+    }
 
     public int getIdEquipe() {
         return idEquipe;
@@ -31,5 +41,9 @@ public class Equipe {
         }
         this.liderEquipe = liderEquipe;
         return true;
+    }
+
+    public void addFuncionario(Funcionario umFuncionario){
+        this.funcionarios.add(umFuncionario);
     }
 }
--- src/main/java/com/management/UnidadeHospitalar.java
+++ src/main/java/com/management/UnidadeHospitalar.java
@@ -1,4 +1,49 @@
 package com.management;
 
+import java.util.ArrayList;
+
 public class UnidadeHospitalar {
+    private String nomeUnidade;
+    private int idUnidadeHospitalar;
+    private String especialidade;
+    private ArrayList<Equipe> equipes;
+
+    UnidadeHospitalar(String nomeUnidade, int idUnidadeHospitalar, String especialidade){
+        this.nomeUnidade = nomeUnidade;
+        this.idUnidadeHospitalar = idUnidadeHospitalar;
+        this.especialidade = especialidade;
+        this.equipes = new ArrayList<Equipe>();
+    }
+
+    public int getIdUnidadeHospitalar() {
+        return idUnidadeHospitalar;
+    }
+
+    public String getNomeUnidade() {
+        return nomeUnidade;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public boolean setNomeUnidade(String nomeUnidade) {
+        if(nomeUnidade.isEmpty()){
+            return false;
+        }
+        this.nomeUnidade = nomeUnidade;
+        return true;
+    }
+
+    public boolean setEspecialidade(String especialidade) {
+        if(especialidade.isEmpty()){
+            return false;
+        }
+        this.especialidade = especialidade;
+        return true;
+    }
+
+    public void addEquipe(){
+
+    }
 }
