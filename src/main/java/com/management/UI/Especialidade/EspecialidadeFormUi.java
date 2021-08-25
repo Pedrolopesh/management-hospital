41b546c43850b2fbd58a14d7306ef56a2459d636
pedrolopeshls <pedrolopeshls@gmail.com>
2021-08-25 17:35:45 -0300

    Tela especialidades

--- src/main/java/com/management/UI/Especialidade/EspecialidadeFormUi.java
+++ src/main/java/com/management/UI/Especialidade/EspecialidadeFormUi.java
@@ -1,10 +1,46 @@
 package com.management.UI.Especialidade;
 
-import javax.swing.*;
+import com.management.Especialidade;
+import com.management.UI.PrincipalUI;
 
-public class EspecialidadeFormUi {
-    private JPanel panel1;
-    private JTextField txNameEspec;
-    private JTextField txIdEspec;
-    private JButton ctnConfirmar;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class EspecialidadeFormUi extends JFrame {
+    private JPanel mainPanel;
+    private JTextField tfNomeEspecialidade;
+    private JButton confirmarButton;
+    private PrincipalUI mainUI;
+
+
+    public EspecialidadeFormUi(PrincipalUI principalUI){
+        this.mainUI = principalUI;
+
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.setContentPane(mainPanel);
+
+
+        confirmarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int nextId = generateId();
+                String nomeEspecialidade = tfNomeEspecialidade.getText();
+
+                Especialidade especialidade = new Especialidade(nomeEspecialidade, nextId);
+
+                salvarDados(especialidade);
+            }
+        });
+    }
+
+
+    private int generateId(){
+        int nextid = this.mainUI.getEspecialidades().size() + 1;;
+        return nextid;
+    }
+
+    private void salvarDados(Especialidade novaEspecialidade){
+        this.mainUI.getEspecialidades().add(novaEspecialidade);
+    }
 }
